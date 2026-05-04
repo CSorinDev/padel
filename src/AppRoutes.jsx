bf8c5e1ffb5964fd6e3eed21db5a40ad258cda77
Sorin C. <csorin.dev@gmail.com>
2026-05-04 23:13:28 +0200

    añadir pagina restantes

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,15 +1,21 @@
-import { Route, Routes } from "react-router";
-import Layout from "./features/layouts/Layout";
-import HomePage from "./pages/HomePage";
+import { Route, Routes } from 'react-router'
+import Layout from './features/layouts/Layout'
+import HomePage from './pages/HomePage'
+import MatchesPage from './pages/MatchesPage'
+import RankingPage from './pages/RankingPage'
+import LoginPage from './pages/LoginPage'
 
 function AppRoutes() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route index element={<HomePage />} />
+        <Route path="partidos" element={<MatchesPage />} />
+        <Route path="clasificacion" element={<RankingPage />} />
+        <Route path="login" element={<LoginPage />} />
       </Route>
     </Routes>
-  );
+  )
 }
 
-export default AppRoutes;
+export default AppRoutes
